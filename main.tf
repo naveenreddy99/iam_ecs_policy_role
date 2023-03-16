bfac43ebdbc3d9e56ac0398326375030b09ac84c
Naveen <tetrs6378@gmail.com>
2023-03-16 06:17:38 -0400

    changes

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-resource "aws_iam_policy" "example_policy" {
+resource "aws_iam_policy" "policy" {
   for_each = fileset(path.module, "policy_documents/*")
   
   name = trimsuffix(basename(each.value), ".json")
@@ -6,10 +6,21 @@ resource "aws_iam_policy" "example_policy" {
 }
 
 
-resource "aws_iam_role" "EksClusterRole" {
+resource "aws_iam_role" "role" {
   for_each = fileset(path.module, "role_documents/*")
   name = trimsuffix(basename(each.value), ".json")
 
   assume_role_policy = file(each.value)
 }
 
+resource "aws_iam_role_policy_attachment" "developer-attach" {
+  count     = length(var.roles["developer"])
+  role       = aws_iam_role.role["role_documents/developer.json"].name
+  policy_arn =  aws_iam_policy.policy["policy_documents/${var.roles["developer"][count.index]}.json"].arn
+}
+
+resource "aws_iam_role_policy_attachment" "devops-attach" {
+  count     = length(var.roles["devops"])
+  role       = aws_iam_role.role["role_documents/devops.json"].name
+  policy_arn =  aws_iam_policy.policy["policy_documents/${var.roles["devops"][count.index]}.json"].arn
+}
